b20f965fd5906e79898b75d901c7acf52aff54bc
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2016-08-04 09:08:48 -0300

    Updating install script

--- setup.sh
+++ setup.sh
@@ -4,44 +4,53 @@ if ! type "sudo" > /dev/null; then
   echo "You do not have 'sudo' installed, so I'll ask your password in every 'sudo' step."
 fi
 
+# sox is for 'play' command to be used with irssi irc client
 echo "01 - Installing basic packages (ruby, git, vim, irc, rdoc, tmux, pip)"
-sudo aptitude install -y ruby rdoc irb git vim python-pip python-pip3 tmux exuberant-ctags
+sudo aptitude install -y ruby rdoc irb git vim python-pip python-pip3 tmux exuberant-ctags sox feh irssi keepass2 chromium zsh
 
-echo "02 - Downloading RubyGems to ~/tools/"
+echo "02 - Changin default shell to zsh"
+chsh -s $(which zsh) $USER
+
+echo "03 - Downloading RubyGems to ~/tools/"
 mkdir -p ~/tools
 cd ~/tools
 wget https://rubygems.org/rubygems/rubygems-2.6.6.tgz
 
-echo "03 - Extracting RubyGems package"
-tar xzf rubygems-2.3.0.tgz
+echo "04 - Extracting RubyGems package"
+tar xzf rubygems-2.6.6.tgz
 
-echo "04 - Installing RubyGems"
-cd rubygems-2.3.0
+echo "05 - Installing RubyGems"
+cd rubygems-2.6.6
 sudo ruby setup.rb all
 
-echo "05 - Updating rubygems"
+echo "06 - Updating rubygems"
 sudo gem update --system
 
-echo "06 - Removing rubygems installer files"
+echo "07 - Removing rubygems installer files"
 cd ..
 rm -rf rubygems*
 
-echo "07 - Installing homesick"
+echo "08 - Installing homesick"
 sudo gem install homesick
 
-echo "08 - Cloning my dotfiles repo"
+echo "09 - Cloning my dotfiles repo"
 homesick clone https://github.com/diraol/dotfiles.git
 
-echo "09 - Updating homesick submodules"
+echo "10 - Updating homesick submodules"
 cd ~/.homesick/repos/dotfiles/
 git checkout personal
 git submodule update --init --recursive
 homesick symlink dotfiles
 
-echo "10 - Installing vim plugins with Bundle/Vundle"
-vim +BundleInstall +BundleClean +qall
-
 # Installing Powerline
 echo "11 - Installing powerline"
 sudo pip install powerline-status
 sudo pip3 install powerline-status
+
+echo "12 - Installing powerline"
+# Installing virtualenv requirements
+sudo pip install virtualenv virtualenvwrapper
+sudo pip3 install virtualenv virtualenvwrapper
+
+echo "13 - Installing vim plugins with Bundle/Vundle"
+vim +BundleInstall +BundleClean +qall
